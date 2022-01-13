7c2969d807b3fdf218cf0ed1df13c1b50d7656d9
Le Vu <levu08072002@gmail.com>
2022-01-14 02:45:25 +0700

    update img

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,7 +1,7 @@
 import data from "../data"
 const Banner = {
     render() {
-        return `<img src="./img/banner.png" />`;
+        return `<img src="https://sathachlaixe.vn/ap-fpt-poly/imager_106846.jpg" />`;
     },
 };
 export default Banner;
